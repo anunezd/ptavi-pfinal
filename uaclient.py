12ffd1039c2c508dfbb0ae94b9afad37981f3ee9
anunezd <alejandro_nd93@hotmail.com>
2018-12-26 11:43:22 +0100

    todo funcionando

--- uaclient.py
+++ uaclient.py
@@ -3,72 +3,88 @@
 
 import sys
 import os
+import socket
 from xml.sax import make_parser
 from xml.sax.handler import ContentHandler
-from proxy_registrar import XMLHandler
+from proxy_registrar import XMLHandler, digest_response, Log_Writer
 
 usage_error = 'usage: python3 uaclient.py config method option'
 method_allowed = ['register', 'invite', 'bye']
+
+
+class SIPMessages:
+
+    def __init__(self, username, ip, port, rtp_audio):
+
+        self.user = username
+        self.address = [ip, port]
+        self.rtp_audio = rtp_audio
+
+    def get_message(self, method, option, digest = ''):
+        if method.lower() == 'register':
+            mess = self.register(option,digest)
+        elif method.lower() == 'invite':
+            mess = self.invite(option)
+        elif method.lower() == 'bye':
+            mess = self.bye(option)
+        elif method.lower() == 'ack':
+            mess = self.ack(option)
+        else:
+            mess = method.upper() + ' ' + self.user + ' SIP/2.0\r\n'
+
+        return mess
+
+    def register(self,option,digest = ''):
+
+        mess = 'REGISTER sip:' + self.user + ':' + self.address[1]
+        mess += ' SIP/2.0\r\nExpires: ' + option
+        if digest != '':
+            mess += '\r\nAuthorization: Digest response="' + digest + '"'
+
+        return mess + '\r\n'
+
+    def invite(self,option):
+
+        mess = 'INVITE sip:' + option + ' SIP/2.0\r\n'
+        mess += 'Content-Type: application/sdp\r\n\r\n'
+        mess += 'v=0\r\no=' + self.user + ' ' + self.address[0]
+        mess += '\r\ns=sesionnueva\r\nt=0\r\n'
+        mess += 'm=audio ' + self.rtp_audio + ' RTP'
+
+        return mess + '\r\n'
+
+    def bye(self,option):
+
+        mess = 'BYE sip:' + option + ' SIP/2.0'
+
+        return mess + '\r\n'
+
+    def ack(self,option):
+
+        mess = 'ACK sip:' + option + ' SIP/2.0'
+
+        return mess + '\r\n'
 
 if len(sys.argv) != 4:
     sys.exit(usage_error)
 else:
     xml_file = sys.argv[1]
     method = sys.argv[2]
-    option = sys.argv[3]
+    if method.lower() == 'register':
+        try:
+            option = int(sys.argv[3])
+        except:
+            sys.exit(usage_error)
+    else:
+        option = sys.argv[3]
 
 config = {'account': ['username', 'passwd'],
-          'username': ['ip', 'puerto'],
-          'rtaudio:': ['puerto'],
+          'uaserver': ['ip', 'puerto'],
+          'rtpaudio': ['puerto'],
           'regproxy': ['ip', 'puerto'],
           'log': ['path'],
           'audio': ['path']}
 
-
-class Log_Writer:
-
-    def __init__(self, log_file, date_format):
-
-        if not os.path.exists(log_file):
-            os.system('touch ' + log_file)
-        self.file = log_file
-        self.date_format= date_format
-
-    def get_date(self):
-
-        return time.strftime((self.date_format),time.gmtime(time.time() + 3600))
-
-    def write(self,line):
-
-        with open(self.file, 'a') as log:
-            log.write(line)
-
-    def starting(self):
-
-        line = self.get_date + ' Starting...'
-        self.write(line)
-
-    def sent_to(self,ip,port,mess):
-
-        line = self.get_date + ' Sent to '
-        line += ip + ':' + port + ': ' + mess.replace('\r\n',' ')
-        self.write(line)
-
-    def received_from(self,ip,port,mess):
-
-        line = self.get_date + ' Received from '
-        line += ip + ':' + port + ': ' + mess.replace('\r\n',' ')
-        self.write(line)
-
-    def error (self,type_error):
-
-        line = self.get_date + ' Error: ' + type_error
-        self.write(line)
-
-    def finishing(self):
-
-        line = self.get_date + ' Finishing...'
-        self.write(line)
 
 parser = make_parser()
 xmlhandler = XMLHandler(config)
@@ -76,3 +92,47 @@ parser.setContentHandler(xmlhandler)
 parser.parse(open(xml_file))
 tags = xmlhandler.get_tags()
 log = Log_Writer(tags['log_path'], '%Y%m%d%H%M%S')
+sip_mess = SIPMessages(tags['account_username'],tags['uaserver_ip'],tags['uaserver_puerto'],tags['rtpaudio_puerto'])
+
+log.starting()
+pr_ip = tags['regproxy_ip']
+pr_port = tags['regproxy_puerto']
+
+with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
+    my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    my_socket.connect((pr_ip, int(pr_port)))
+    line = sip_mess.get_message(method,str(option))
+    print("Enviando: " + line)
+    log.sent_to(pr_ip,pr_port,line.replace('\r\n', ' '))
+    my_socket.send(bytes(line, 'utf-8') + b'\r\n')
+    try:
+        data = my_socket.recv(1024).decode('utf-8')
+    except:
+        log.error('No server listening at ' + pr_ip + ' port ' + pr_port)
+        sys.exit('Conection refused')
+    if data != '':
+        log.received_from(pr_ip,pr_port,data.replace('\r\n', ' '))
+        print(data)
+        if '401' in data:
+            # enviar digest response
+            passwd = tags['account_passwd']
+            nonce = data.split('"')[1]
+            line = sip_mess.get_message(method,str(option),digest_response(nonce,passwd))
+            log.sent_to(pr_ip,pr_port,line.replace('\r\n', ' '))
+            my_socket.send(bytes(line, 'utf-8') + b'\r\n')
+            try:
+                data = my_socket.recv(1024).decode('utf-8')
+                print(data)
+                log.received_from(pr_ip,pr_port,data.replace('\r\n', ' '))
+            except:
+                log.error('No server listening at ' + pr_ip + ' port ' + pr_port)
+                sys.exit('Conection refused')
+        elif '100' and '180' and '200' in data:
+            # envia ack
+            line = sip_mess.get_message('ack',option)
+            my_socket.send(bytes(line, 'utf-8') + b'\r\n')
+            log.sent_to(pr_ip,pr_port,line.replace('\r\n', ' '))
+        else:
+            print(data.replace('\r\n',' '))
+
+log.finishing()
