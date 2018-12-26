9998e8c3f93fae5eec4dfe47f244339aeffd1766
anunezd <alejandro_nd93@hotmail.com>
2018-12-26 13:34:41 +0100

    pasado pep8

--- uaserver.py
+++ uaserver.py
@@ -13,6 +13,7 @@ from proxy_registrar import XMLHandler, Log_Writer
 
 FORMATO = '%Y%m%d%H%M%S'
 
+
 class EchoHandler(socketserver.DatagramRequestHandler):
     mp32rtp = []
 
@@ -20,7 +21,7 @@ class EchoHandler(socketserver.DatagramRequestHandler):
         line = self.rfile.read().decode('utf-8')
         ip = self.client_address[0]
         port = self.client_address[1]
-        log.received_from(ip,str(port),line.replace('\r\n',' '))
+        log.received_from(ip, str(port), line.replace('\r\n', ' '))
         print(line)
         if 'INVITE' in line:
             ip_dst = line.split('\r\n')[4].split()[-1]
@@ -36,8 +37,10 @@ class EchoHandler(socketserver.DatagramRequestHandler):
             mess += '\r\ns=sesionnueva\r\nt=0\r\n'
             mess += 'm=audio ' + tags['rtpaudio_puerto'] + ' RTP\r\n\r\n'
         elif 'ACK' in line:
-            cvlc = 'cvlc rtp://@ ' + self.mp32rtp[0] + ':' + self.mp32rtp[1] + ' & '
-            cvlc += './mp32rtp -i ' + self.mp32rtp[0] + ' -p ' + self.mp32rtp[1]
+            ip = self.mp32rtp[0]
+            puerto = self.mp32rtp[1]
+            cvlc = 'cvlc rtp://@ ' + ip + ':' + puerto + ' & '
+            cvlc += './mp32rtp -i ' + ip + ' -p ' + puerto
             cvlc += ' < ' + tags['audio_path']
             os.system(cvlc)
             if self.mp32rtp != '':
@@ -47,9 +50,10 @@ class EchoHandler(socketserver.DatagramRequestHandler):
             mess = 'SIP\2.0 200 OK\r\n\r\n'
         else:
             mess = 'SIP\2.0 405 Method Not Allowed\r\n\r\n'
-        if not 'ACK' in line:
-            self.wfile.write(bytes(mess,'utf-8'))
-            log.sent_to(ip,str(port),mess.replace('\r\n',' '))
+        if 'ACK' not in line:
+            self.wfile.write(bytes(mess, 'utf-8'))
+            log.sent_to(ip, str(port), mess.replace('\r\n', ' '))
+
 
 if __name__ == "__main__":
     config = {'account': ['username', 'passwd'],
@@ -68,7 +72,7 @@ if __name__ == "__main__":
     parser.setContentHandler(xmlhandler)
     parser.parse(open(xml_file))
     tags = xmlhandler.get_tags()
-    log = Log_Writer(tags['log_path'],FORMATO)
+    log = Log_Writer(tags['log_path'], FORMATO)
     ip = tags['uaserver_ip']
     puerto = int(tags['uaserver_puerto'])
     serv = socketserver.UDPServer((ip, puerto), EchoHandler)
