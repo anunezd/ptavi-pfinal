3553a9c9595a57a7a2a9340abf46afafe7527ac6
anunezd <alejandro_nd93@hotmail.com>
2018-12-26 13:13:20 +0100

    pasado pep8

--- proxy_registrar.py
+++ proxy_registrar.py
@@ -15,21 +15,24 @@ from xml.sax.handler import ContentHandler
 FORMATO = '%Y%m%d%H%M%S'
 
 config = {'server': ['name', 'ip', 'puerto'],
-          'database': ['path','passwdpath'],
+          'database': ['path', 'passwdpath'],
           'log': ['path']}
+
 
 def digest_nonce(username):
     digest = sha1()
-    digest.update(bytes(username,'utf-8'))
+    digest.update(bytes(username, 'utf-8'))
     digest.digest()
     return digest.hexdigest()
 
-def digest_response(nonce,passwd):
+
+def digest_response(nonce, passwd):
     digest = sha1()
-    digest.update(bytes(nonce,'utf-8'))
-    digest.update(bytes(passwd,'utf-8'))
+    digest.update(bytes(nonce, 'utf-8'))
+    digest.update(bytes(passwd, 'utf-8'))
     digest.digest()
     return digest.hexdigest()
+
 
 class Log_Writer:
 
@@ -38,13 +41,14 @@ class Log_Writer:
         if not os.path.exists(log_file):
             os.system('touch ' + log_file)
         self.file = log_file
-        self.date_format= date_format
+        self.date_format = date_format
 
     def get_date(self):
 
-        return time_log.strftime((self.date_format),time_log.gmtime(time_log.time() + 3600))
+        now = time_log.gmtime(time_log.time() + 3600)
+        return time_log.strftime((self.date_format), now)
 
-    def write(self,line):
+    def write(self, line):
 
         with open(self.file, 'a') as log:
             log.write(line + '\n')
@@ -54,19 +58,19 @@ class Log_Writer:
         line = self.get_date() + ' Starting...'
         self.write(line)
 
-    def sent_to(self,ip,port,mess):
+    def sent_to(self, ip, port, mess):
 
         line = self.get_date() + ' Sent to '
-        line += ip + ':' + port + ': ' + mess.replace('\r\n',' ')
+        line += ip + ':' + port + ': ' + mess.replace('\r\n', ' ')
         self.write(line)
 
-    def received_from(self,ip,port,mess):
+    def received_from(self, ip, port, mess):
 
         line = self.get_date() + ' Received from '
-        line += ip + ':' + port + ': ' + mess.replace('\r\n',' ')
+        line += ip + ':' + port + ': ' + mess.replace('\r\n', ' ')
         self.write(line)
 
-    def error (self,type_error):
+    def error(self, type_error):
 
         line = self.get_date() + ' Error: ' + type_error
         self.write(line)
@@ -79,7 +83,7 @@ class Log_Writer:
 
 class XMLHandler(ContentHandler):
 
-    def __init__(self,conf):
+    def __init__(self, conf):
 
         self.list = {}
         self.dtd = conf
@@ -93,6 +97,7 @@ class XMLHandler(ContentHandler):
     def get_tags(self):
         return self.list
 
+
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
 
     dicc = {}
@@ -104,10 +109,10 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
         line = self.rfile.read().decode('utf-8')
         ip = self.client_address[0]
         port_src = self.client_address[1]
-        log.received_from(ip,str(port_src),line.replace('\r\n',' '))
+        log.received_from(ip, str(port_src), line.replace('\r\n', ' '))
         doc = line.split('\r\n')
         print(line)
-        if not 'SIP/2.0' in line:
+        if 'SIP/2.0' not in line:
             bad_request = True
         else:
             bad_request = False
@@ -121,7 +126,7 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
                     try:
                         del self.dicc[user]
                         self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-                        log.sent_to(ip,str(port_src),'SIP/2.0 200 OK')
+                        log.sent_to(ip, str(port_src), 'SIP/2.0 200 OK')
                     except KeyError:
                         pass
                 else:
@@ -130,24 +135,27 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
                     fecha = expired.strftime(FORMATO)
                     self.dicc[user] = {'Address': address, 'Expires': fecha}
                     self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-                    log.sent_to(ip,str(port_src),'SIP/2.0 200 OK')
+                    log.sent_to(ip, str(port_src), 'SIP/2.0 200 OK')
             else:
-                #401 unathorized
-                if not 'Digest response' in line:
+                # 401 unathorized
+                if 'Digest response' not in line:
                     line = 'SIP/2.0 401 Unathorized\r\nWWW Authenticate: '
                     line += 'Digest nonce="' + digest_nonce(user) + '"\r\n\r\n'
-                    self.wfile.write(bytes(line,'utf-8'))
-                    log.sent_to(ip,str(port_src), line.replace('\r\n',' '))
+                    self.wfile.write(bytes(line, 'utf-8'))
+                    log.sent_to(ip, str(port_src), line.replace('\r\n', ' '))
                 else:
-                    dig_response = digest_response(digest_nonce(user),self.passwd[user])
+                    nonce = digest_nonce(user)
+                    passwd = self.passwd[user]
+                    dig_response = digest_response(nonce, passwd)
                     dig_response_user = line.split('"')[1]
                     if dig_response == dig_response_user:
                         expired = tiempo + timedelta(seconds=int(expires))
                         address = str(self.client_address[0]) + ":" + port
                         fecha = expired.strftime(FORMATO)
-                        self.dicc[user] = {'Address': address, 'Expires': fecha}
+                        self.dicc[user] = {'Address': address,
+                                           'Expires': fecha}
                         self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
-                        log.sent_to(ip,str(port_src),'SIP/2.0 200 OK')
+                        log.sent_to(ip, str(port_src), 'SIP/2.0 200 OK')
                     else:
                         pass
         elif 'INVITE' or 'BYE' or 'ACK' in doc[0] and not bad_request:
@@ -159,18 +167,19 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
                 print(res)
                 if res != '':
                     print('upsie')
-                    self.wfile.write(bytes(res,'utf-8'))
-                    log.sent_to(ip,str(port_src),res.replace('\r\n',' '))
+                    self.wfile.write(bytes(res, 'utf-8'))
+                    log.sent_to(ip, str(port_src), res.replace('\r\n', ' '))
                     print('upsie')
         else:
             if not bad_request:
-                #405 method not allowed
+                # 405 method not allowed
                 self.wfile.write(b"SIP/2.0 405 Method not Allowed\r\n\r\n")
-                log.sent_to(ip,str(port_src),'SIP/2.0 405 Method not Allowed')
+                log.sent_to(ip, str(port_src),
+                            'SIP/2.0 405 Method not Allowed')
             else:
                 # enviar 400 Bad Request
                 self.wfile.write(b"SIP/2.0 400 Bad Request\r\n\r\n")
-                log.sent_to(ip,str(port_src),'SIP/2.0 400 Bad Request')
+                log.sent_to(ip, str(port_src), 'SIP/2.0 400 Bad Request')
 
         self.register2json()
 
@@ -179,12 +188,12 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
             my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
             my_socket.connect((ip, int(puerto)))
             print("Enviando: " + line)
-            log.sent_to(ip,puerto,line.replace('\r\n', ' '))
+            log.sent_to(ip, puerto, line.replace('\r\n', ' '))
             my_socket.send(bytes(line, 'utf-8'))
             try:
                 data = my_socket.recv(1024).decode('utf-8')
-                log.received_from(ip,str(puerto),data.replace('\r\n', ' '))
-            except:
+                log.received_from(ip, str(puerto), data.replace('\r\n', ' '))
+            except ConnectionRefusedError:
                 data = ''
         return data
 
@@ -198,8 +207,9 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
                 self.dicc = json.load(reg_json)
             with open(tags['database_passwdpath'], 'r') as passwd_json:
                 self.passwd = json.load(passwd_json)
-        except:
+        except FileNotFoundError:
             pass
+
 
 if __name__ == "__main__":
 
@@ -212,7 +222,7 @@ if __name__ == "__main__":
     parser.setContentHandler(xmlhandler)
     parser.parse(open(xml_file))
     tags = xmlhandler.get_tags()
-    log = Log_Writer(tags['log_path'],FORMATO)
+    log = Log_Writer(tags['log_path'], FORMATO)
     ip = tags['server_ip']
     puerto = int(tags['server_puerto'])
     serv = socketserver.UDPServer((ip, puerto), SIPRegisterHandler)
