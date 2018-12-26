aa8d133e67b3a38346ab1e504a94136faaae602f
anunezd <alejandro_nd93@hotmail.com>
2018-12-26 13:34:46 +0100

    pasado pep8

--- uaclient.py
+++ uaclient.py
@@ -20,9 +20,9 @@ class SIPMessages:
         self.address = [ip, port]
         self.rtp_audio = rtp_audio
 
-    def get_message(self, method, option, digest = ''):
+    def get_message(self, method, option, digest=''):
         if method.lower() == 'register':
-            mess = self.register(option,digest)
+            mess = self.register(option, digest)
         elif method.lower() == 'invite':
             mess = self.invite(option)
         elif method.lower() == 'bye':
@@ -34,7 +34,7 @@ class SIPMessages:
 
         return mess
 
-    def register(self,option,digest = ''):
+    def register(self, option, digest=''):
 
         mess = 'REGISTER sip:' + self.user + ':' + self.address[1]
         mess += ' SIP/2.0\r\nExpires: ' + option
@@ -43,7 +43,7 @@ class SIPMessages:
 
         return mess + '\r\n'
 
-    def invite(self,option):
+    def invite(self, option):
 
         mess = 'INVITE sip:' + option + ' SIP/2.0\r\n'
         mess += 'Content-Type: application/sdp\r\n\r\n'
@@ -53,17 +53,18 @@ class SIPMessages:
 
         return mess + '\r\n'
 
-    def bye(self,option):
+    def bye(self, option):
 
         mess = 'BYE sip:' + option + ' SIP/2.0'
 
         return mess + '\r\n'
 
-    def ack(self,option):
+    def ack(self, option):
 
         mess = 'ACK sip:' + option + ' SIP/2.0'
 
         return mess + '\r\n'
+
 
 if len(sys.argv) != 4:
     sys.exit(usage_error)
@@ -73,7 +74,7 @@ else:
     if method.lower() == 'register':
         try:
             option = int(sys.argv[3])
-        except:
+        except ValueError:
             sys.exit(usage_error)
     else:
         option = sys.argv[3]
@@ -92,7 +93,11 @@ parser.setContentHandler(xmlhandler)
 parser.parse(open(xml_file))
 tags = xmlhandler.get_tags()
 log = Log_Writer(tags['log_path'], '%Y%m%d%H%M%S')
-sip_mess = SIPMessages(tags['account_username'],tags['uaserver_ip'],tags['uaserver_puerto'],tags['rtpaudio_puerto'])
+acc_user = tags['account_username']
+s_ip = tags['uaserver_ip']
+s_puerto = tags['uaserver_puerto']
+rtp_puerto = tags['rtpaudio_puerto']
+sip_mess = SIPMessages(acc_user, s_ip, s_puerto, rtp_puerto)
 
 log.starting()
 pr_ip = tags['regproxy_ip']
@@ -101,38 +106,43 @@ pr_port = tags['regproxy_puerto']
 with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as my_socket:
     my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
     my_socket.connect((pr_ip, int(pr_port)))
-    line = sip_mess.get_message(method,str(option))
+    line = sip_mess.get_message(method, str(option))
     print("Enviando: " + line)
-    log.sent_to(pr_ip,pr_port,line.replace('\r\n', ' '))
+    log.sent_to(pr_ip, pr_port, line.replace('\r\n', ' '))
     my_socket.send(bytes(line, 'utf-8') + b'\r\n')
     try:
         data = my_socket.recv(1024).decode('utf-8')
-    except:
+    except ConnectionRefusedError:
         log.error('No server listening at ' + pr_ip + ' port ' + pr_port)
         sys.exit('Conection refused')
     if data != '':
-        log.received_from(pr_ip,pr_port,data.replace('\r\n', ' '))
-        print(data)
+        log.received_from(pr_ip, pr_port, data.replace('\r\n', ' '))
         if '401' in data:
             # enviar digest response
             passwd = tags['account_passwd']
             nonce = data.split('"')[1]
-            line = sip_mess.get_message(method,str(option),digest_response(nonce,passwd))
-            log.sent_to(pr_ip,pr_port,line.replace('\r\n', ' '))
+            digest_response = digest_response(nonce, passwd)
+            line = sip_mess.get_message(method, str(option), digest_response)
+            log.sent_to(pr_ip, pr_port, line.replace('\r\n', ' '))
             my_socket.send(bytes(line, 'utf-8') + b'\r\n')
             try:
                 data = my_socket.recv(1024).decode('utf-8')
                 print(data)
-                log.received_from(pr_ip,pr_port,data.replace('\r\n', ' '))
-            except:
-                log.error('No server listening at ' + pr_ip + ' port ' + pr_port)
+                log.received_from(pr_ip, pr_port, data.replace('\r\n', ' '))
+            except ConnectionRefusedError:
+                no_ser_list = 'No server listening at '
+                log.error(no_ser_list + pr_ip + ' port ' + pr_port)
                 sys.exit('Conection refused')
-        elif '100' and '180' and '200' in data:
-            # envia ack
-            line = sip_mess.get_message('ack',option)
-            my_socket.send(bytes(line, 'utf-8') + b'\r\n')
-            log.sent_to(pr_ip,pr_port,line.replace('\r\n', ' '))
+        elif '200' in data:
+            if '100' in data:
+                if '180' in data:
+                    # envia ack
+                    line = sip_mess.get_message('ack', option)
+                    my_socket.send(bytes(line, 'utf-8') + b'\r\n')
+                    log.sent_to(pr_ip, pr_port, line.replace('\r\n', ' '))
+            else:
+                print(data.replace('\r\n', ' '))
         else:
-            print(data.replace('\r\n',' '))
+            print(data.replace('\r\n', ' '))
 
 log.finishing()
