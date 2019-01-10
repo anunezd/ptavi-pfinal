48de1815c6a32f651cb30ad425c425fba7a85c66
anunezd <alejandro_nd93@hotmail.com>
2019-01-10 11:18:34 +0100

    corregido error vlc

--- uaserver.py
+++ uaserver.py
@@ -37,7 +37,7 @@ class EchoHandler(socketserver.DatagramRequestHandler):
         elif 'ACK' in line:
             ip = self.mp32rtp[0]
             puerto = self.mp32rtp[1]
-            cvlc = 'cvlc rtp://@ ' + ip + ':' + puerto + ' & '
+            cvlc = 'cvlc rtp://@' + ip + ':' + puerto + ' & '
             cvlc += './mp32rtp -i ' + ip + ' -p ' + puerto
             cvlc += ' < ' + tags['audio_path']
             os.system(cvlc)
